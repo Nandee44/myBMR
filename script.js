a2eaa2edea8aea445217cf543849b6e85396a09f
Kawtar <kawtarziadi6@gmail.com>
2026-08-22 22:02:48 +0100

    fixed age limit

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 document.getElementById("form").addEventListener("submit", function(event) {
+    console.log("hi");
     event.preventDefault();
 
-    const age = document.getElementById("age").value;
-    const height = document.getElementById("height").value;
-    const weight = document.getElementById("weight").value;
+    const age = parseFloat(document.getElementById("age").value);
+    const height = parseFloat(document.getElementById("height").value);
+    const weight = parseFloat(document.getElementById("weight").value);
     const gender = document.querySelector('input[name="gender"]:checked').value;
+
+    if (age === null || height === null || weight === null || gender === null) {
+        
+    }
+
+    console.log(age + weight + height);
+
+
 
 });
