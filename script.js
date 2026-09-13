0e4ab072d75b5f857b2986625a421df24c0711ce
Kawtar <kawtarziadi6@gmail.com>
2026-09-13 20:16:37 +0100

    calculate and display bmr

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function calculateBmr(A,H,W,G) {
+    let bmr;
+    if (G == "male"){
+        bmr = 10*W + 6.25*H - 5*A + 5;
+    } else {
+        bmr = 10*W + 6.25*H - 5*A - 161;
+    }
+    return bmr;
+}
+
+function displayBmr(bmr) {
+    //insert result message in p element with id result
+    let result = document.getElementById("result");
+    result.textContent = "Your BMR is " + bmr + " calories per day"
+}
+
+
+// after submitting form
 document.getElementById("form").addEventListener("submit", function(event) {
     console.log("hi");
     event.preventDefault();
@@ -7,12 +25,13 @@ document.getElementById("form").addEventListener("submit", function(event) {
     const weight = parseFloat(document.getElementById("weight").value);
     const gender = document.querySelector('input[name="gender"]:checked').value;
 
-    if (age === null || height === null || weight === null || gender === null) {
-        
-    }
+    console.log(age, weight, height);
 
-    console.log(age + weight + height);
+    // need to handle missing or incorrect input
 
+    let bmr = calculateBmr(age, height, weight, gender);
+    console.log(bmr);
 
+    displayBmr(bmr);
 
 });
